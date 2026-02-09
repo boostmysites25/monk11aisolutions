8d6b162db74e3e33d07ab678d3869bd07e616e36
Pranav <mpranavprem@gmail.com>
2026-02-09 14:07:47 +0530

    fix build errors

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -155,7 +155,7 @@ const ContactUs = () => {
                     {...register("email", {
                       required: "Email is required",
                       pattern: {
-                        value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                        value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
                         message: "Entered email is invalid",
                       },
                     })}
